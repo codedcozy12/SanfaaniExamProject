0b4feac838a03ebbb42670cbf15daf7592eb5b0b
Abdulkabir fagbohun <122563248+abdulkabir18@users.noreply.github.com>
2025-06-11 18:54:10 +0100

    Removing usings

--- Doman/Entities/Instructor.cs
+++ Doman/Entities/Instructor.cs
@@ -1,11 +1,4 @@
-﻿using Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Entities
+﻿namespace Domain.Entities
 {
     public class Instructor
     {
